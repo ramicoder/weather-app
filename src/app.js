63e0255e79e2c23f19776541dba0306a31b4d4da
ramicoder <ramidaood@anonymous>
2026-07-17 20:45:57 +0300

    coded minimal design of the app including the app title, form, and submit button

--- src/app.js
+++ src/app.js
@@ -3,18 +3,30 @@ import "./styles.css";
 import "./styles.css";
 
 
-async function getDate(location) {
+async function getData(location) {
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=HGJ4HFVFYRFL3SUXDF6ZEHXVW&contentType=json`
         );
-        const data = await response.json();
         if (!response.ok) {
-            throw new Error("`HTTP error! Status: ${response.status}`");
+            throw new Error(`HTTP error! Status: ${response.status}`);
         }
+        const data = await response.json();
         console.log(data);
+        return data;
     } catch(error) {
-        console.error("Fetch operation failed.", error);
+        console.error("Fetch operation failed: ", error);
     }
 }
 
-getDate("baghdad");
+getData("Erbil").then(data => {
+    console.log(processData(data));
+});
+
+function processData(data) {
+        const datetime = data.currentConditions.datetime;
+        const humidity = data.currentConditions.humidity;
+        const temp = data.currentConditions.temp;
+
+        return { datetime, humidity, temp };
+}
+
