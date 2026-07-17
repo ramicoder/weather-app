7286686b6db83d64a6e3411b209cc9325c7c1a60
ramicoder <ramidaood@anonymous>
2026-07-17 19:28:27 +0300

    coded some fetching logic using async/await to log the fdata to the console

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 import "./styles.css";
+
+import "./styles.css";
+
+
+async function getDate(location) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=HGJ4HFVFYRFL3SUXDF6ZEHXVW&contentType=json`
+        );
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error("`HTTP error! Status: ${response.status}`");
+        }
+        console.log(data);
+    } catch(error) {
+        console.error("Fetch operation failed.", error);
+    }
+}
+
+getDate("baghdad");
